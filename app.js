02450116295f3d1da2ce3573f132845b817c539e
Chigorin M.L <mmchigorin@gmail.com>
2019-05-17 14:00:42 +0300

    add json data and components

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const data = require('./data');
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.listen(port)
+
+app.use('/assets', express.static(__dirname + '/assets'))
+
+//setting routes
+app.get('/', (req, res) => { 
+
+  res.render('index', {data})
+  res.send(data.seals)
+
+})
+
+app.get('/seal-profile/:name', (req, res) => { 
+  res.render('seal-profile')
+})
+
+app.get('/seals', (req, res) => { 
+  res.render('seals')
+})
+
+app.get('/facts', (req, res) => { 
+  res.render('facts')
+})
